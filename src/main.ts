53892510b20b6ed537332a2fbcba664c9133940e
Youssef <youssef.afify122@gmail.com>
2021-07-04 13:07:29 +0200

    Update .env

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './core/app.module';
 import { ValidationPipe } from "@nestjs/common";
-
+require('dotenv').config({
+  path: __dirname+'/../.env'
+})
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
